7922c5e432968f39e6fd03b2cf083b6475138e13
Rodrigo Sartori <rodrigo.sartori1544@gmail.com>
2023-11-20 22:54:58 -0300

    Aprendizado de .NET C#. Aulas a partir de Matriz

---
+++ Matriz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0;  j < n; j++)
+                {
+
+                }
+            }
+            
+        }
+    }
+}
